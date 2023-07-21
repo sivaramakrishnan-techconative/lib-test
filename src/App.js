e51a8325504f6a027257441bbb3de5ff2f2feca4
sivaramakrishnan-techconative <sivaramakrishnan.srivatsan@techconative.com>
2023-07-21 09:43:57 +0530

    srk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [value, setvalue] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={value} />
+      <button type="button" onClick={() => setvalue("Value Changed")}>
+        Change value
+      </button>
     </div>
   );
 }
